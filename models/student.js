eb8da0ded34d779298038d4ff085a7d85e64981f
Arvind6353 <arvindan92@gmail.com>
2017-10-09 19:09:43 +0530

    model chanegs

--- models/student.js
+++ models/student.js
@@ -1,10 +1,13 @@
 var mongoose = require('mongoose')
 
 var studentSchema = mongoose.Schema({
-    studentname: { type: 'String', required: true, unique: true }
+    name: { type: 'String', required: true, unique: true },
+    age: { type: 'number' },
+    address: { type: 'String' },
+    hobbies: { type: 'String' },
+    gender: { type: 'String' }
+
 }, { timestamp: true })
 
 
 module.exports = mongoose.model('student', studentSchema, 'students')
- 
- 
